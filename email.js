ec6a7d600077833c7adf7e68dfca125c5c9329a3
Nyaco <catpacross@gmail.com>
2021-01-29 14:01:36 +0900

    review and commition modal add

---
+++ email.js
@@ -0,0 +1,18 @@
+const accept = document.getElementById('accetp');
+const submit = document.getElementById('submit');
+(function(){
+    emailjs.init("user_6W7H9JW9czHKpNYZnOCUw");
+ })();
+
+ submit.addEventListener('click',()=>{
+    emailjs.send('service_6vt0j4e', 'template_udyobb6', {
+        email: document.getElementById('input-email').value,
+        type: document.getElementById('web').value
+    }).then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('외주 요청이 정상적으로 완료되었습니다. 최대한 빠른 시일내에 답변드리겠습니다.');
+    }, function(error) {
+        console.log('FAILED...', error);
+        alert('외주 요청에 실패하였습니다. 다시 시도하거나 직접 연락해주세요.');
+})});
+
