59f3e5ee4b0f6afb2f52799c97baf74ef37a1f27
Nyaco <catpacross@gmail.com>
2021-02-15 22:01:29 +0900

    style edit & email form edit

--- email.js
+++ email.js
@@ -1,10 +1,11 @@
-const accept = document.getElementById('accetp');
 const submit = document.getElementById('submit');
+const form = document.getElementById('form');
 (function(){
     emailjs.init("user_6W7H9JW9czHKpNYZnOCUw");
  })();
 
- submit.addEventListener('click',()=>{
+function sendEmail(event){
+    event.preventDefault();
     emailjs.send('service_6vt0j4e', 'template_udyobb6', {
         email: document.getElementById('input-email').value,
         type: document.getElementById('web').value
@@ -14,5 +15,8 @@ const submit = document.getElementById('submit');
     }, function(error) {
         console.log('FAILED...', error);
         alert('외주 요청에 실패하였습니다. 다시 시도하거나 직접 연락해주세요.');
-})});
+    });
+};
 
+ 
+ form.addEventListener('submit', sendEmail);
